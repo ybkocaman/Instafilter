93a198d5fb42d855ea5d4e4e90752c546e8c19b2
Yusuf <ybkocaman@gmail.com>
2023-12-29 16:48:33 +0000

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Yusuf Burak on 29/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
